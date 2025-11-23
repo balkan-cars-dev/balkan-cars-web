768ad71368abc5e1e61bf86030f863db83a5976b
georgi-g-likov <georgilikov2@gmail.com>
2025-11-23 22:59:33 +0200

    Added login panel

--- src/app/core/body-component/body-component.ts
+++ src/app/core/body-component/body-component.ts
@@ -2,13 +2,14 @@ import {Component, Input} from '@angular/core';
 import { CarPartsPanel } from '../../structure/car-parts-panel/car-parts-panel';
 import { CarPanel } from '../../structure/car-panel/car-panel';
 import { BlogPanel } from '../../structure/blog-panel/blog-panel';
+import { LoginComponent } from "../login-component/login.component";
 
 @Component({
   selector: 'app-body-component',
-  imports: [CarPanel, CarPartsPanel, BlogPanel],
+  imports: [CarPanel, CarPartsPanel, BlogPanel, LoginComponent],
   templateUrl: './body-component.html',
-  styleUrl: './body-component.scss',
+  styleUrls: ['./body-component.scss'],
 })
 export class BodyComponent {
-  @Input() selected: 'vehicles' | 'carparts' | 'blog' = 'vehicles';
+  @Input() selected: 'vehicles' | 'carparts' | 'blog' | 'login' = 'vehicles';
 }
---
+++ src/app/core/login-component/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../login-component/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => this.router.navigate(['/cars']),
+      error: (err) => this.errorMessage = 'Invalid email or password'
+    });
+  }
+}
