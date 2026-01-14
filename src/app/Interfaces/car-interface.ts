30935a18328f48cc8f3e630e61da6d130b338aab
georgi-g-likov <georgilikov2@gmail.com>
2026-01-14 23:33:46 +0200

    Added filtering
    added filtering to the front-end and extended carListing interface

--- src/app/Interfaces/car-interface.ts
+++ src/app/Interfaces/car-interface.ts
@@ -1,13 +1,16 @@
 export interface CarListing {
   id: string;
-  make: string;
+  brand: string;
   model: string;
   details: string;
-  priceEur: number;
-  priceBgn: number;
+  price: number;
   mileage: number;
   region: string;
   date: string;
   imageUrl: string;
+  year: string;
+  state: string;
+  fuel: string;
+  transmission: string;
   badge?: 'BEST' | 'TOP' | string;
 }
